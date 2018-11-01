72c5fa41d806f5bc858a7c57c45af0c28616c84e
Konstantin Perikov <pericov.ko@gmail.com>
2018-11-01 16:16:53 +0000

    add test skeleton

--- src/main/kotlin/com/github/galleyltd/boost/service/AnalysisSerivce.kt
+++ src/main/kotlin/com/github/galleyltd/boost/service/AnalysisSerivce.kt
@@ -14,6 +14,7 @@ class SimpleAnalysisService : AnalysisSerivce {
     private val STEP = 1
 
     override fun analyse(matches: List<MatchData>, accountId: Int): Boolean {
+        println()
         val xpmData = matches.map { it.players?.first { it -> it?.accountId == accountId }?.xpPerMin }
             .map { it -> it!! }
         val gpmData = matches.map { it.players?.first { it -> it?.accountId == accountId }?.goldPerMin }
@@ -23,6 +24,9 @@ class SimpleAnalysisService : AnalysisSerivce {
         val apmData = matches.map { it.players?.first { it -> it?.accountId == accountId }?.actionsPerMin }
             .map { it -> it!! }
         val movingAverageXPM = xpmData.windowed(WINDOW_SIZE, STEP) { it.average() }
+        val movingAverageGPM = gpmData.windowed(WINDOW_SIZE, STEP) { it.average() }
+        val movingAverageKPM = kpmData.windowed(WINDOW_SIZE, STEP) { it.average() }
+        val movingAverageAPM = apmData.windowed(WINDOW_SIZE, STEP) { it.average() }
         println(movingAverageXPM)
         return false
     }
--- src/test/kotlin/com/github/galleyltd/boost/service/AnalysisServiceTest.kt
+++ src/test/kotlin/com/github/galleyltd/boost/service/AnalysisServiceTest.kt
@@ -1,28 +1,47 @@
 package com.github.galleyltd.boost.service
 
+import com.github.galleyltd.boost.di.KoinContainer
 import com.github.galleyltd.boost.opendota.dto.MatchData
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertTrue
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
-import kotlin.random.Random
 
 class AnalysisServiceTest {
 
+    private val koinContainer = KoinContainer().also { it.init() }
+    private val openDotaApiClient = koinContainer.openDotaApiClient
     private val service = SimpleAnalysisService()
 
-    private val matches: List<MatchData> = generateTestMatches()
-
-    private fun generateTestMatches(): List<MatchData> {
-        val size = Random.nextInt(10, 100)
-        for (i <- 0 until size) {
-
+    private suspend fun requestTestMatches(ids: List<String>): List<MatchData> {
+        val matches = mutableListOf<MatchData>()
+        for (id in ids) {
+            matches.add(openDotaApiClient.getMatchData(id))
         }
+        return matches
     }
 
     @Test
     fun testWindowedAvg() {
+        val matches = runBlocking {
+            requestTestMatches(
+                listOf(
+                    "3361901428",
+                    "3443905190",
+                    "3981938989",
+                    "4040513022",
+                    "4087082146",
+                    "4173573070",
+                    "4173654727",
+                    "4174586560",
+                    "4174904803",
+                    "4176511736",
+                    "4176574180"
+                )
+            )
+        }
+
+        service.analyse(matches, 116894024)
         assertTrue(false)
     }
 }
